8edd329cf7bf8eed38924ace07142999d4512896
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-05-24 12:00:42 -0600

    createGrid functions

---
+++ script.js
@@ -0,0 +1,31 @@
+let gridInput = 'How large would you like the grid. /nLimit 100';
+let btnClicked = false;
+let gridSize = 16;
+let loop = 0;
+
+
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) { 
+        loop += 1;
+        console.log('loop', loop);
+                const bodyDiv = document.querySelector('.body');
+        let gridElement = document.createElement('div');
+        gridElement.innerText = ('div' + loop);
+        bodyDiv.appendChild(gridElement);
+        if (loop < (gridSize * gridSize)) {
+            for (let i = gridSize; i > loop; i--) {
+                createGrid();
+                console.log('loopy', loop);
+            }
+        }
+    }
+}
+
+function sizeButton() {
+    if (btnClicked) {
+        console.log(gridSize);
+
+    }
+}
+
+createGrid();
