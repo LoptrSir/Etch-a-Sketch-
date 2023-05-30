37a408937bac8edf8aa9d64f6e7c1c1d6029a823
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-05-30 09:59:48 -0600

    Grid created

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 let gridInput = 'How large would you like the grid. /nLimit 100'; //create this as a prompt after button is clicked
 let btnClicked = false;
-let gridSize = 16;
-let loop = 0;
-
+let gridSize = 8;
+let totalElements = gridSize * gridSize;
 
 function createGrid() {
-    for (let i = 0; i < gridSize; i++) { 
-        loop += 1;
-        console.log('loop', loop);
-                const bodyDiv = document.querySelector('.body');
-        let gridElement = document.createElement('div');
-        gridElement.innerText = ('div' + loop);
-        bodyDiv.appendChild(gridElement);
-        if (loop < (gridSize * gridSize)) {
-            for (let i = gridSize; i > loop; i--) {
-                createGrid();
-                console.log('loopy', loop);
-            }
-        }
-    }
+  const bodyDiv = document.querySelector('.body');
+
+  for (let i = 0; i < totalElements; i++) {
+    const gridElement = document.createElement('div');
+    const elementIndex = i + 1;
+    gridElement.innerText = 'div' + elementIndex;
+
+    // unable to find a clean simple way to migrate this to css without allot of code
+    gridElement.style.flexBasis = `calc(100% / ${gridSize})`;
+
+    bodyDiv.appendChild(gridElement);
+  }
 }
 // Working on Button
 function sizeButton() {
 
-    if (btnClicked) {
-        prompt(gridInput);
-        gridSize = prompt;
-        console.log('GS', gridSize, 'btn click');
+  if (btnClicked) {
+    prompt(gridInput);
+    gridSize = prompt;
+    console.log('GS', gridSize, 'btn click');
 
-    }
+  }
 }
-const sizeBtn = document.querySelector('.size');
-sizeBtn.addEventListener('click', sizeButton);
+const sizeBtn = document.getElementById('FSize');
+sizeBtn.onclick = sizeButton(); // consider changing to an anonymous function in the onclick
 createGrid();
 // end of Button
 
