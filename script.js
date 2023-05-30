34a9e6543d48f8081616eeba13209c245cec4483
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-05-30 11:21:35 -0600

    FieldSize button functions

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-let gridInput = 'How large would you like the grid. /nLimit 100'; //create this as a prompt after button is clicked
-let btnClicked = false;
+let gridInput = ('How large would you like the grid? Limit 68');
 let gridSize = 8;
 let totalElements = gridSize * gridSize;
 
 function createGrid() {
   const bodyDiv = document.querySelector('.body');
-
+  bodyDiv.innerHTML = '';
   for (let i = 0; i < totalElements; i++) {
     const gridElement = document.createElement('div');
     const elementIndex = i + 1;
     gridElement.innerText = 'div' + elementIndex;
-
     // unable to find a clean simple way to migrate this to css without allot of code
     gridElement.style.flexBasis = `calc(100% / ${gridSize})`;
-
     bodyDiv.appendChild(gridElement);
   }
 }
-// Working on Button
-function sizeButton() {
 
-  if (btnClicked) {
-    prompt(gridInput);
-    gridSize = prompt;
-    console.log('GS', gridSize, 'btn click');
-
-  }
+function handleEvent() {
+  gridSize = prompt(gridInput);
+  totalElements = gridSize * gridSize;
+  console.log('GS', gridSize, 'btn click');
+  createGrid()
 }
-const sizeBtn = document.getElementById('FSize');
-sizeBtn.onclick = sizeButton(); // consider changing to an anonymous function in the onclick
-createGrid();
-// end of Button
+
+FSize.addEventListener('click', handleEvent);
 
 createGrid();
