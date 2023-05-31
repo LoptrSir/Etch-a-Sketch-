015052347d793a05545de2e0436b684ff773cae7
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-05-31 11:56:02 -0600

    cleanup comments and console.logs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let gridInput = ('How large would you like the grid? Limit 68');
 let gridSize = 8;
 let totalElements = gridSize * gridSize;
 
+// Grid generation
 const bodyDiv = document.querySelector('.body');
 function createGrid() {
   bodyDiv.innerHTML = '';
@@ -9,12 +10,13 @@ function createGrid() {
     const gridElement = document.createElement('div');
     const elementIndex = i + 1;
     gridElement.innerText = elementIndex;
-    // unable to find a clean simple way to migrate this to css without allot of code
+    // unable to find a clean way to place into css.
     gridElement.style.flexBasis = `calc(100% / ${gridSize})`;
     bodyDiv.appendChild(gridElement);
   }
 }
 
+//FieldSize button functionality
 FSize.addEventListener('click', function () {
   let inputValid = false;
   while (!inputValid) {
@@ -26,14 +28,14 @@ FSize.addEventListener('click', function () {
       alert('ERROR: Input a valid number 1 -68');
     }
   }
-  console.log('GS', gridSize, 'btn click');
   totalElements = gridSize * gridSize;
   createGrid()
 });
 
+// color change function
 bodyDiv.addEventListener('mouseover', function(event){
   if (event.target.matches('.body div')) {
-    event.target.style.backgroundColor = 'blue'; // Change the background color to blue 
+    event.target.style.backgroundColor = 'blue'; 
 }});
 
 createGrid();
